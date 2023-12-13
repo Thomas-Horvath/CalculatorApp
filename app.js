b37f2f917acf272bf62d455268a1192e0f59e62a
Thomas-Horvath <thomas.horvath0416@gmail.com>
2023-12-13 21:11:00 +0100

    create division function

--- app.js
+++ app.js
@@ -5,20 +5,47 @@ const display_output = document.querySelector(".display .output");
 let input = "";
 
 for (var key of keys) {
-    const value = key.dataset.key;
+    let value = key.dataset.key;
     key.addEventListener('click', () => {
         console.log(value);
+
+
         if (value === "clear") {
             input = "";
             display_input.innerHTML = 0;
             display_output.innerHTML = 0;
-        } else if (value === "delete") {
-            input = input.slice(0, -1);
-            display_input.innerHTML = input;
-        } else if (value === "=") {
-            let result = eval(input);
+        }
+
+
+        else if (value === "delete") {
+            if (input === "") {
+                display_input.innerHTML = 0;
+            }
+            else {
+                input = input.slice(0, -1);
+                display_input.innerHTML = input === "" ? 0 : input;
+            }
+        }
+
+
+        else if (value === "=") {
+            let result;
+
+            // Ellenőrizzük, hogy az input tartalmazza-e a '%' jelet
+            if (input.includes('%')) {
+                // Ha van '%' jel, akkor az értéket a háttérben százalékké alakítjuk
+                result = eval(input.replace("%", "/100*"));
+            } else {
+                // Ha nincs '%' jel, akkor normál értékkiértékelést végzünk
+                result = eval(input);
+            }
+
             display_output.innerHTML = result;
-        } else if (value === "brackets") {
+        }
+
+
+
+        else if (value === "brackets") {
             if (
                 input.indexOf('(') == -1 ||
                 input.indexOf(')') != -1 &&
@@ -37,12 +64,13 @@ for (var key of keys) {
             }
 
             display_input.innerHTML = input;
-        } else {
-         input += value;
-         display_input.innerHTML = input;
-         /* let result = eval(input);
-            display_output.innerHTML = result; */
-        
+        }
+
+
+
+        else {
+            input += value;
+            display_input.innerHTML = input;
         }
     })
 }   
